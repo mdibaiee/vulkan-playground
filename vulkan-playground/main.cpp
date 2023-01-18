387037fbd6e5ed0b6dde0c8fb4d87cba9d0ea3f6
Mahdi Dibaiee <mdibaiee@pm.me>
2023-01-18 10:12:44 +0000

    Initial Commit

---
+++ vulkan-playground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  vulkan-playground
+//
+//  Created by Mahdi Dibaiee on 18/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
